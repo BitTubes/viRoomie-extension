bdc508ded65085e2494ae8781a888a40e187f2a7
Jörn Berkefeld <joern.berkefeld@gmail.com>
2015-08-11 17:36:48 +0200

    safari extension files

---
+++ safari/viroomie.safariextension/load_video_content_script.js
@@ -0,0 +1,37 @@
+/* jshint strict:false */
+
+
+// var additionalInfo = {
+//   "title": document.title,
+//   "selection": window.getSelection().toString()
+// };
+// document.getElementById("m")
+
+// chrome.runtime.connect().postMessage(additionalInfo);
+// chrome.runtime.onConnect.addListener(function(port) {
+
+// });
+
+chrome.runtime.onMessage.addListener(function(message, sender, callback) {
+	console.log("message from popup.js:", message);
+	switch(message.a) {
+		case "state":
+			callback("to-do");
+		break;
+		case "url":
+			var m = document.getElementById("m");
+			m.value = "/video "+message.url;
+			var b = document.getElementById("send");
+			if(b.click) {
+				b.click();
+			}
+			m.focus();
+			callback(true);
+		break;
+		default:
+		console.log("unknown state:", message.a);
+			callback(false);
+
+	}
+
+});
