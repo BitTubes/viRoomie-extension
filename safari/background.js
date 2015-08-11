23e9100d1c25d3f3ffeb081e8823c91b3ddaf0e9
Jörn Berkefeld <joern.berkefeld@gmail.com>
2015-08-11 12:50:43 +0200

    safari extension init with chrome files

---
+++ safari/background.js
@@ -0,0 +1,172 @@
+/* jshint strict:false */
+
+
+
+//  +++++++++++++++ GOOGLE ANALYTICS ++++++++++++
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-65099990-5']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
+
+chrome.runtime.onConnect.addListener(function(port) {
+  var tab = port.sender.tab;
+
+  // This will get called by the content script we execute in
+  // the tab as a result of the user pressing the browser action.
+  port.onMessage.addListener(function(info) {
+    var max_length = 1024;
+    if (info.selection.length > max_length){
+      info.selection = info.selection.substring(0, max_length);
+    }
+    executeMailto(tab.id, info.title, tab.url, info.selection);
+  });
+});
+
+
+
+var path19 = "img/icon19_share.png";
+// var path38 = "img/icon19_share.png";
+
+// wrapper because setting the icon directly is currently not possible... bugreport pending
+function createSetIconAction(callback) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var image = new Image();
+  image.onload = function() {
+    ctx.drawImage(image,0,0,19,19);
+    var imageData = ctx.getImageData(0,0,19,19);
+    var action = new chrome.declarativeContent.SetIcon({imageData: imageData});
+    callback(action);
+  };
+  image.src = chrome.runtime.getURL(path19);
+}
+// function createSetIconAction(callback) {
+//   var canvas = document.createElement("canvas");
+//   var ctx = canvas.getContext("2d");
+//   var image19 = new Image();
+//   image19.onload = function() {
+//     ctx.drawImage(image,0,0,19,19);
+//     var imageData19 = ctx.getImageData(0,0,19,19);
+//     var image38 = new Image();
+//     image38.onload = function() {
+//       ctx.drawImage(image,0,0,38,38);
+//       var imageData38 = ctx.getImageData(0,0,38,38);      
+//       var action = new chrome.declarativeContent.SetIcon({"imageData":{
+//         "19": imageData19,
+//         "38": imageData38
+//       }});
+//       callback(action);
+//     };
+//     image38.src = chrome.runtime.getURL(path38);
+//   };
+//   image19.src = chrome.runtime.getURL(path19);
+// }
+
+// function setPageActionTitle() {
+//   chrome.tabs.query(queryInfo, function(tabs) {
+//     var currentTab = tabs[0];
+//     chrome.pageAction.setTitle({
+//       tabId: currentTab.id,
+//       title: chrome.i18n.getMessage("application_title")
+//     });
+//   });
+// }
+var embeddableYt = {};
+chrome.extension.onConnect.addListener(function(port) {
+  console.log("Connected .....");
+  port.onMessage.addListener(function(data) {
+    switch(data.a) {
+      case "getEmbeddableYt":
+        port.postMessage({
+          "a":"embeddableYt",
+          "id": data.id,
+          "val":embeddableYt[data.id]
+        });
+        break;
+      case "setEmbeddableYt":
+        embeddableYt[data.id] = data.val;
+        break;
+    }
+  });
+});
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    createSetIconAction(function(setIconAction) {
+      chrome.declarativeContent.onPageChanged.addRules([
+        // {
+        //   conditions: [
+        //     // That fires when a page's URL contains a 'chrome' ...
+        //     new chrome.declarativeContent.PageStateMatcher({
+        //       pageUrl: { urlContains: 'chrome.com' },
+        //     // }),
+        //     // // When a page contains a <video> tag...
+        //     // new chrome.declarativeContent.PageStateMatcher({
+        //     //   css: ["video"]
+        //     })
+        //   ],
+        //   // And shows the extension's page action.
+        //   actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        // },
+        {
+          conditions: [
+            // That fires when a page's URL contains a 'chrome' ...
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { 
+                hostEquals: 'www.youtube.com'//,
+                // pathPrefix: '/watch'
+              },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        },
+        {
+          conditions: [
+            // That fires when a page's URL contains a 'chrome' ...
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { 
+                hostEquals: 'nlv.bittubes.com',
+                queryContains: 'uid='
+              },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        // },
+        // {
+        //   conditions: [
+        //     // That fires when a page's URL contains a 'chrome' ...
+        //     // setPageActionTitle(),
+        //     new chrome.declarativeContent.PageStateMatcher({
+        //       pageUrl: { hostEquals: 'app.viroomie.com' },
+        //     })
+        //   ],
+        //   // And shows the extension's page action.
+        //   actions: [ 
+        //     // new chrome.declarativeContent.SetIcon({
+        //     //    path : {
+        //     //     "19" : "img/icon19_share.png",
+        //     //     "38" : "img/icon38_share.png"
+        //     //   }
+        //     // }),
+        //     setIconAction,
+        //     new chrome.declarativeContent.ShowPageAction()
+        //   ]
+        }
+      ]); // addRules
+    }); // createSetIconAction
+  }); // removeRules
+}); // addListener
---
+++ safari/popup.js
@@ -0,0 +1,271 @@
+/* jshint strict:false */
+
+var _ = chrome.i18n.getMessage;
+var p_updateapps;
+var p_openapp;
+var p_msg;
+
+//  +++++++++++++++ GOOGLE ANALYTICS ++++++++++++
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-65099990-5']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
+// //  +++++++++++++++ EMAIL INVITE ++++++++++++
+
+// function sendMail() {
+//   chrome.tabs.executeScript(null, {file: "email_content_script.js"});
+// }
+
+
+
+//  +++++++++++++++ OPEN VIDEO ++++++++++++
+function _addElement(url, room, tabId, windowId) { 
+  // create a new div element 
+  // and give it some content 
+  var newButton = document.createElement("button"); 
+  var newContent = document.createTextNode(_("open_in_existing", [room]));
+  // newButton.setAttribute("data-room", room);
+  newButton.setAttribute("id", "tabId"+tabId);
+  newButton.onclick = updateViroom.bind(null,url, tabId, windowId);
+  newButton.appendChild(newContent); //add the text node to the newly created div. 
+
+  document.getElementById("updateapps").appendChild(newButton);
+}
+function _getRoomFromHash(url) {
+  if(url.indexOf("#")>=0) {
+    var hashvar,
+      hashvars = url.split("#")[1].split("&");
+    for (var i = hashvars.length - 1; i >= 0; i--) {
+      hashvar = hashvars[i].split("=");
+      if(hashvar[0] == "room") {
+        return hashvar[1];
+      }
+    }
+  }
+  return false;
+}
+function _getYouTubeId(url) {
+  var uriFormats = ["/embed/","watch?v=","youtu.be/","youtube.com/v/"];
+  for (var i = 0; i < uriFormats.length; i++) {
+    if(url.indexOf(uriFormats[i])!=-1) {
+      return (url.split(uriFormats[i]))[1].substr(0,11);
+      // break;
+    }
+  }
+  return false;
+}
+
+
+function openViRoomie(url) {
+  chrome.tabs.create( {url: "http://app.viroomie.com#video="+(url.split("=").join("%3D").split("&").join("%26"))} );
+}
+function updateViroom(url, tabId, windowId) {
+  // chrome.tabs.executeScript(tabId, {file: "load_video_content_script.js"});
+  chrome.tabs.sendMessage(tabId, {"a":"url", "url":url}, function(data) {
+    console.log("response from content-script: ",data);
+    chrome.tabs.update(tabId, { // make tab active in its window
+      active:true
+    }, function() {
+      if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError.message);
+          var button = document.getElementById("tabId"+tabId);
+          button.parentNode.removeChild(button);
+      } else {
+        chrome.windows.update(windowId, { // make tab's window active
+          drawAttention:true, // blink
+          focused:true // bring window to front
+        }, function() {});
+        window.close();
+      }
+    });
+  });
+}
+
+function findViRoomieTabs(url) {
+  var buttonWrapper = p_updateapps;
+  buttonWrapper.setAttribute("data-msg", _("searching_rooms"));
+
+  var queryInfo = {
+    status: "complete",
+    url: "http://app.viroomie.com/*"
+  };
+  var room,
+    roomCounter = 0,
+    tab;
+  chrome.tabs.query(queryInfo, function(tabs) {
+    buttonWrapper.removeAttribute("data-msg");
+    urls = "";
+    for(var el in tabs) {
+      tab = tabs[el];
+      room = _getRoomFromHash(tab.url);
+      if(room) {
+        roomCounter++;
+        _addElement(url, room, tab.id, tab.windowId);
+      }
+    }
+    if(roomCounter) {
+      buttonWrapper.setAttribute("data-or", _("or"));
+    } else {
+      buttonWrapper.style.display = "none";
+    }
+  });
+
+}
+
+
+
+//  +++++++++++++++ UTIL ++++++++++++
+// var _currentTabId = null;
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var currentTab = tabs[0];
+    // _currentTabId = currentTab.id;
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = currentTab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+  // chrome.tabs.getCurrent(function(currentTab){
+  //   callback(currentTab.url);
+  // });
+  // Most methods of the Chrome extension APIs are asynchronous!
+}
+function processUrl(url, viroomieCallback, externalCallback) {
+  if(url.indexOf("//app.viroomie.com")>0) {
+    viroomieCallback(url);
+  } else {
+    externalCallback(url);
+  }
+}
+var embeddableYtCallback;
+var port = chrome.extension.connect({name: "Sample Communication"});
+// port.postMessage("Hi BackGround");
+port.onMessage.addListener(function(data) {
+  switch(data.a) {
+    case "embeddableYt":
+      embeddableYtCallback(data.id, data.val);
+      break;
+  }
+});
+
+
+function checkEmbedStatus(url, callback) {
+  var ytID = _getYouTubeId(url);
+  // console.log(embeddableYt[ytID]);
+  embeddableYtCallback = function(embeddableYt_ID, embeddableYt_Val) {
+    if(embeddableYt_ID!=ytID) {
+      return;
+    }
+    if("undefined" != typeof embeddableYt_Val) {
+      callback(embeddableYt_Val);
+      return;
+    }
+    var ApiKey = 'AIzaSyDVaU9_EkkYi7OZLCb2XANCLLO7zGfDjpA'; // API key for chrome extension; if you re-use this code, please change
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE ) {
+        if(xhr.status == 200){
+          var data = JSON.parse(xhr.responseText);
+          try {
+            data = data["items"][0]["status"]["embeddable"];
+          } catch(e) {
+            data = false;
+          }
+          embeddableYt_Val = data;
+          callback(data);
+        }
+        else { // fallback - something went wrong with that api call hence we assume embeddable to be true
+          embeddableYt_Val = true;
+          callback(true);
+        }
+      }
+    };
+
+    xhr.open("GET", 'https://www.googleapis.com/youtube/v3/videos?part=status&id='+ytID+'&maxResults=1&fields=items(status%2Fembeddable)&key='+ApiKey, true);
+    xhr.send();
+  };
+  port.postMessage({
+    "a": "getEmbeddableYt",
+    "id": ytID
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  p_updateapps = document.getElementById('updateapps');
+  p_openapp = document.getElementById('openapp');
+  p_msg = document.getElementById('msg');
+  // document.getElementById('options').onclick = function() {
+  //   if (chrome.runtime.openOptionsPage) {
+  //     // New way to open options pages, if supported (Chrome 42+).
+  //     chrome.runtime.openOptionsPage();
+  //   } else {
+  //     // Reasonable fallback.
+  //     window.open(chrome.runtime.getURL('options.html'));
+  //   }
+  // };
+  function showTabs(url) {
+    p_openapp.innerHTML = _("open_new");
+    findViRoomieTabs(url);
+  }
+  getCurrentTabUrl(function(url) {
+    processUrl(url, function() { // inside viRoomie
+
+      // p_msg.innerHTML = "app:"+url;
+      // document.getElementById('app').style.display = "block";
+
+      // document.getElementById('screenshot').onclick = makeScreenshot;
+      // document.getElementById('email').onclick = sendMail;
+
+    }, function(url) { // external website
+      
+      if(url.indexOf("youtube.com/watch")>0) {
+        checkEmbedStatus(url,function(embeddable) {
+          if(embeddable) {
+            showTabs(url);
+          } else {
+            p_msg.innerHTML = _("embed_video_error");
+            p_updateapps.style.display = "none";
+            p_openapp.style.display = "none";
+          }
+        });
+      } else if(url.indexOf("//nlv.bittubes.com")>=0) {
+        showTabs(url);
+      } else {
+        p_msg.innerHTML = _("open_video_error");
+        p_updateapps.style.display = "none";
+        p_openapp.style.display = "none";
+      }
+      document.getElementById('external').style.display = "block";
+      p_openapp.onclick = openViRoomie.bind(null,url);
+    });
+  });
+});
