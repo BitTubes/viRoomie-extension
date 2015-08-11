4915eaab89f8a4de3120e3e925e008fd8fea76c1
Jörn Berkefeld <joern.berkefeld@gmail.com>
2015-08-11 19:52:12 +0200

    finalised injected script, switched to firefox code base for background.js and injected script

--- safari/viroomie.safariextension/load_video_content_script.js
+++ safari/viroomie.safariextension/load_video_content_script.js
@@ -1,37 +1,34 @@
 /* jshint strict:false */
 
 
-// var additionalInfo = {
-//   "title": document.title,
-//   "selection": window.getSelection().toString()
-// };
-// document.getElementById("m")
+if (window.top === window) {
+	function handleMessage(message) {
+		var callback_msg;
+		switch(message.a) {
+			case "state":
+				callback_msg = "to-do";
 
-// chrome.runtime.connect().postMessage(additionalInfo);
-// chrome.runtime.onConnect.addListener(function(port) {
+			break;
+			case "url":
+				try {
+					var m = document.getElementById("m");
+					m.value = "/video "+message.url;
+					var b = document.getElementById("send");
+					if(b.click) {
+						b.click();
+					}
+					m.focus();
+					callback_msg = true;
+				} catch(e) {
+					callback_msg = false;
+				}
+			break;
+			default:
+				console.log("unknown state:", message.a);
+				callback_msg = false;
 
-// });
-
-chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-	console.log("message from popup.js:", message);
-	switch(message.a) {
-		case "state":
-			callback("to-do");
-		break;
-		case "url":
-			var m = document.getElementById("m");
-			m.value = "/video "+message.url;
-			var b = document.getElementById("send");
-			if(b.click) {
-				b.click();
-			}
-			m.focus();
-			callback(true);
-		break;
-		default:
-		console.log("unknown state:", message.a);
-			callback(false);
-
+		}
+		safari.self.tab.dispatchMessage("cb_"+message.a,callback_msg);
 	}
-
-});
+	safari.self.addEventListener("message", handleMessage, false);
+}
